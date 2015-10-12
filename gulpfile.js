40b1b96e389d02f45a76859dec8e06f1b7f23882
T.Kostiuchenko <mydylan7@gmail.com>
2015-10-12 10:39:51 +0300

    start building page

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,21 @@
-var gulp = require("gulp"),
+var gulp = require('gulp'),
     connect = require('gulp-connect'),
     concat = require('gulp-concat'),
     clean = require('gulp-clean'),
     rename = require('gulp-rename'),
     replace = require('gulp-replace'),
     sass = require('gulp-sass'),    
+    autoprefixer = require('gulp-autoprefixer'),
     csslint = require('gulp-csslint'),
     wrap = require('gulp-wrap'),
-    slm = require('gulp-slm');
+    slm = require('gulp-slm'),
+    link = "../src/assets";
     
 gulp.task('clean', function () {
     return gulp.src('./dist/**/*').pipe(clean());
 });
 
-gulp.task("jsbuild", ['clean'], function() {
+gulp.task('jsbuild', ['clean'], function() {
     return gulp.src('./src/js/*.js')
         .pipe(concat('main.js'))
         .pipe(gulp.dest('./dist/'));
@@ -22,6 +24,9 @@ gulp.task("jsbuild", ['clean'], function() {
 gulp.task('sass', ['clean'], function () {
     return gulp.src('./src/css/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(replace("_link_", link))
+        .pipe(concat('main.scss'))
+        .pipe(autoprefixer())
         .pipe(rename({suffix: '.min'}))
         .pipe(rename({extname: '.css'}))
         .pipe(gulp.dest('./dist/'));
@@ -30,7 +35,12 @@ gulp.task('sass', ['clean'], function () {
 gulp.task('slm', ['clean'], function () {
     return gulp.src('./src/view/*.slim')
         .pipe(slm())
-        .pipe(gulp.dest("./dist/"));
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('source', ['clean'], function () {
+    return gulp.src('./src/assets/*.*')
+        .pipe(gulp.dest('./dist/assets/'));
 });
 
 gulp.task('connect', function() {
@@ -40,13 +50,13 @@ gulp.task('connect', function() {
     });
 });
 
-gulp.task('reload', function() {
-    gulp.src('./dist/index.html')
+gulp.task('reload', ['clean', 'slm', 'jsbuild', 'sass'], function() {
+    gulp.src('./dist/main.html')
         .pipe(connect.reload());
 });
 
 gulp.task('watch', function() {
-    gulp.watch('src/**/*.*', ['clean', 'slm', 'jsbuild', 'sass', 'reload']);
+    gulp.watch('src/**/*.*', ['reload']);
 });
 
 
